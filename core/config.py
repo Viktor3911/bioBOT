4fe89fe1f0c5471a72bfc7054884d9da5035daed
Сергей Прокопенко <psi200523@gmail.com>
2025-03-01 19:11:22 +1000

    config.py

--- core/config.py
+++ core/config.py
@@ -2,9 +2,6 @@ PG_FSM_DBNAME = "bio_fsm_state"
 PG_DBNAME = "bio"
 
 PG_USER = "postgres"
-<<<<<<< HEAD
 PG_PASSWORD = "2112"
-=======
->>>>>>> 7aebace608b47859eecc67d33553ebb32589e99b
 PG_HOST = "localhost"
 PG_PORT = 5432
